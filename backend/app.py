8beb3d33e93abfb243c1523296a4fca6d8b2bac1
sitiisyaroh <102467384+sitiisyaroh@users.noreply.github.com>
2025-04-30 21:34:57 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -34,4 +34,5 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Default ke 5000 untuk local run
+    app.run(debug=True, host='0.0.0.0', port=port)
